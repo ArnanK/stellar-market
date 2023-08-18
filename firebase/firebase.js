65834d7fdee73bbd97f02ed4f33fdd53aa0ab970
ArnanK <70865851+ArnanK@users.noreply.github.com>
2023-08-18 18:02:29 -0400

    Update firebase.js
    security for key,
    
    replace in .env.local file

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,8 +5,11 @@ import { getFirestore } from 'firebase/firestore'
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-
-    
+    process.env.AUTH0_SECRET,
+    process.env.AUTH0_BASE_URL,
+    process.env.AUTH0_ISSUER_BASE_URL,
+    process.env.AUTH0_CLIENT_ID,
+    process.env.AUTH0_CLIENT_SECRET,
 }
 
 const app = initializeApp(firebaseConfig)
